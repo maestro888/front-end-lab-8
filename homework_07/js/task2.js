0b374e7e7c12c73ec7e8d28eff78651ec6712f0b
maestro888ua <maestro888ua@gmail.com>
2018-02-14 07:13:00 +0200

    continue homework_07

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,64 +1,68 @@
 var range = 5;
-var randomNum = 0;
-var attempt = 0;
+var attempt = 1;
 var round = 1;
-var startPrize = 10;
-var prize = 10;
-var message;
-var startMessage = 'Do you want to play a game?';
+var totalPrize = 0;
+var startPrise = 10;
+var prize = startPrise;
 var endMessage = 'Thank you for a game. Your prize is: ';
 var againMessage = 'Dou you want to play again?';
-var info, myNum;
-var currentPrize;
+var continueMessage = 'Do you want to continue a game';
+var randomNum, myNum, info, message;
 
-randomNum = Math.floor(Math.random() * (range + 1));
-console.log('Random no '+ randomNum);
+message = confirm('Do you want to play a game?');
 
-while (attempt < 3) {
-    console.log(prize);
-    console.log(attempt + 1);
-    info = 'Attempts left: ' + (attempt + 1) + '\n' + 'Total prize: ' + prize + '$\n' + 'Possible prize: ' + prize / (attempt + 1) + '$';
-    myNum = prompt(info + '\n\nPlease enter a number', '');
-
-    if (myNum == null) {
-        break;
-    }
-
-    if (myNum == randomNum) {
-        prize = prize / (attempt + 1);
-        console.log(endMessage + prize + '$');
-        break;
-    }
-
-    attempt++;
-}
-
-message = confirm(againMessage);
-
-if (message == true) {
-    startPrize = startPrize * 3;
-    range = range * 2;
-    randomNum = Math.floor(Math.random() * (range + 1));
-
-    attempt = 0;
-    while (attempt < 3) {
-        info = 'Attempts left: ' + (attempt + 1) + '\n' + 'Total prize: ' + prize + '$\n' + 'Possible prize: ' + prize / (attempt + 1) + '$';
-        myNum = prompt(info + '\n\nPlease enter a number', '');
-
-        if (myNum == null) {
-            break;
-        }
-
-        if (myNum == randomNum) {
-            prize = prize / (attempt + 1);
-            console.log(endMessage + prize + '$');
-            break;
-        }
-
-        attempt++;
-    }
-
+if (!message) {
+    console.log('You did not become a millionaire');
 } else {
+
+    outer: for (;round; round++) {
+        
+        attempt = 1;
+        range = range * 2;
+        startPrise = startPrise * 3;
+        randomNum = Math.floor(Math.random() * (range + 1));
+        // console.log('Random no '+ randomNum);
+
+        for (;attempt <= 3;) {
+            info = 'Round: ' + round +
+                '\nAttempts left: ' + attempt +
+                '\nTotal prize: ' + totalPrize + '$' +
+                '\nPossible prize: ' + prize + '$';
+
+            myNum = prompt(info + '\n\nPlease enter a number', '');
+
+            if (!myNum || myNum.match(/^[0-9]+$/) == null) {
+                break outer;
+            }
+
+            if (myNum == randomNum) {
+                console.log(endMessage + prize + '$');
+                totalPrize = totalPrize + prize;
+
+                message = confirm(continueMessage);
+                if (message == true) {
+                    prize = startPrise;
+                    continue outer;
+                } else {
+                    break outer;
+                }
+            }
+
+            if (attempt == 3) {
+                message = confirm(againMessage);
+                if (message == true) {
+                    prize = startPrise + totalPrize;
+                    continue outer;
+                } else {
+                    break outer;
+                }
+            }
+
+            prize = prize / 2;
+
+            attempt++;
+        }
+    }
+
     console.log(endMessage + '…');
 }
-
